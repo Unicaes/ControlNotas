523c4e8b02f781adfb1b4283810fd9ec1551c78d
Bryan Palma <bryanmauriciopalma@hotmail.com>
2021-05-26 08:59:18 -0600

    Nuevo modelo para gestion de porcentajes globales

---
+++ ApiAgiles/Controllers/PeriodoController.cs
@@ -0,0 +1,57 @@
+﻿using ApiAgiles.Models;
+using ApiAgiles.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiAgiles.Controllers
+{
+    public class PeriodoController:ApiController
+    {
+        private readonly ICrudGeneral<Periodo> db = new RPeriodo();
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            var resp = db.Get();
+            if (!resp.IsSuccess)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, resp.Message);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, resp);
+        }
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            var resp = db.GetById(id);
+            if (!resp.IsSuccess)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, resp.Message);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, resp);
+        }
+        [HttpPost]
+        public HttpResponseMessage Post(Periodo item)
+        {
+            var resp = db.Post(item);
+            if (!resp.IsSuccess)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, resp.Message);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, resp);
+        }
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            var resp = db.Delete(id);
+            if (!resp.IsSuccess)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, resp.Message);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, resp);
+        }
+    }
+}
---
+++ ApiAgiles/Repository/RPeriodo.cs
@@ -0,0 +1,140 @@
+﻿using ApiAgiles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAgiles.Repository
+{
+    public class RPeriodo : ICrudGeneral<Periodo>
+    {
+        private readonly Model1 db = new Model1();
+        public Response Delete(int id)
+        {
+            try
+            {
+                var user = db.Periodo.Find(id);
+                if (user == null)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = "No se encontro ningun Aula con el id " + id
+                    };
+                }
+                var resp = db.Periodo.Remove(user);
+                db.SaveChanges();
+                if (resp == null)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = "El Periodo no existe"
+                    };
+                }
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = user,
+                    Message = "El Periodo ha sido eliminado exitosamente"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.InnerException.Message
+                };
+            }
+        }
+
+        public Response Get()
+        {
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                var periodos = db.Periodo.ToList();
+                if (periodos.Count()<=0)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = "No data in the table"
+                    };
+                }
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = periodos
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess=false,
+                    Message=ex.Message
+                };
+            }
+        }
+
+        public Response GetById(int id)
+        {
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                var periodos = db.Periodo.Find(id);
+                if (periodos==null)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = "The record does not exist"
+                    };
+                }
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = periodos
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public Response Post(Periodo item)
+        {
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                db.Periodo.Add(item);
+                db.SaveChanges();
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = item
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.InnerException.Message
+                };
+            }
+        }
+
+        public Response Put(Periodo item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
